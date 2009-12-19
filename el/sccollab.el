b1024810c5b23d33697369408f77fadbdffd9c22
Justin Glenn Smith <justin@idiopathic.(none)>
2009-12-18 18:46:05 -0800

    still fleshing out, chaos

--- el/sccollab.el
+++ el/sccollab.el
@@ -4,10 +4,15 @@
        sccollab-clients
        get-ip)
   (setq get-ip (arg ip)
+	"get an ip address"
 	(interactive "s\add ip: "))
   (defun sccollab (arg ip-list) 
     "start a supercollider collaboration"
     (interactive)
-    (setq sccollab-clients 
+    (unless ip-list
+      (let (new-ip)
+	(while (setq new-ip (get-ip))
+	  (setq ip-list (cons new-ip ip-list)))))
+    (setq sccollab-clients
 	  (mapcar (lambda (client-ip) (osc-make-client client-ip 7777))
 		  ip-list))))
