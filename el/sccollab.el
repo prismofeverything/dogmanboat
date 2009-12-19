766b6ea412c202d8beae592e3eff8faaac1fd895
Justin Glenn Smith <justin@idiopathic.(none)>
2009-12-18 18:31:00 -0800

    working on interactive elements

--- el/sccollab.el
+++ el/sccollab.el
@@ -1,8 +1,8 @@
 (require 'osc)
 
-(let* ((sccollab-server (osc-make-server "localhost" 7777)))
-  (defun sccollab (iplist) ()))
-
-
-
-
+(let* ((sccollab-server (osc-make-server "localhost" 7777))
+       get-ip)
+  (setq get-ip (arg ip)
+	(interactive "s\add ip: "))
+  (defun sccollab (arg iplist)
+    (interactive)))
