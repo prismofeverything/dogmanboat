b29224e1da92cb5287482cc95e8b2fb91fc5ce13
Justin Glenn Smith <justin@idiopathic.(none)>
2009-12-18 19:09:28 -0800

    wackiness with bindings

--- el/sccollab.el
+++ el/sccollab.el
@@ -1,23 +1,20 @@
 (require 'osc)
 
-
-(let* (sccollab-server
-       sccollab-clients
-       get-ip)
-  (setq get-ip (arg ip)
-	"get an ip address"
-	(interactive "s\add ip: "))
-  (defun start-sccollab-server ()
-    (setq sccollab-server (osc-make-server "localhost" 7777 
-                                           (lambda (path &rest args)
-                                             (receive-collaboration path args)))))
-  (defun sccollab (arg ip-list)
-    "start a supercollider collaboration"
-    (interactive)
-    (unless ip-list
-      (let (new-ip)
-	(while (setq new-ip (get-ip))
-	  (setq ip-list (cons new-ip ip-list)))))
-    (setq sccollab-clients
-	  (mapcar (lambda (client-ip) (osc-make-client client-ip 7777))
-		  ip-list))))
+(defvar sccollab-server)
+(defvar sccollab-clients)
+(defun start-sccollab-server ()
+  (setq sccollab-server
+	(osc-make-server "localhost" 7777 
+			 (lambda (path &rest args)
+			   (receive-collaboration path args)))))
+(defun sccollab (&optional ip-list)
+  "start a supercollider collaboration"
+  (interactive)
+  (unless ip-list
+    (let (new-ip)
+      (while (setq new-ip (read-from-minibuffer "add ip: "))
+	(setq ip-list (cons new-ip ip-list))
+	(display-warning :debug ip-list)))
+  (setq sccollab-clients
+	(mapcar (lambda (client-ip) (osc-make-client client-ip 7777))
+		ip-list))))
