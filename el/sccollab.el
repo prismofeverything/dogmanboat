2dac00d6d4d5f1fb71101fc2322d9934f33461ad
Justin Glenn Smith <justin@information-density.(none)>
2009-12-19 14:19:59 -0800

    interactive choice of server interface, with prompt showing list of those availible

--- el/sccollab.el
+++ el/sccollab.el
@@ -4,14 +4,39 @@
 (defvar sccollab-clients)
 
 (defun sccollab-server-start (&optional addr)
-  "serve sccollab to people connecting at addr"
-  (interactive
-   "Mour ip - specify localhost or 127.0.0.1 for local connections only: ")
-					; how to get ip of default address?
-  (setq sccollab-server
-	(osc-make-server addr 7777 
-			 (lambda (path &rest args)
-			   (sccollab-receive path args)))))
+  "serve sccollab to people connecting at addrs"
+  (interactive)
+  (unless addr
+    ;; here we are making a list of network devices, and prompting the user
+    ;; to select one, we then use the ip address of that device
+    (setq addr
+	  (let* (find-default-device default-device-name
+				     (interfaces (network-interface-list))
+				     (prompt "device: ("))
+	    (let ((search-interfaces interfaces))
+	      ;; find the first interface that is not loopback, or settle for
+	      ;; loopback
+	      (while (not default-device-name)
+		(if (not search-interfaces)
+		    (setq default-device-name "lo")
+		  (if (not (string= (caar search-interfaces) "lo"))
+		      (setq default-device-name (caar search-interfaces)))
+		  (setq search-interfaces (cdr search-interfaces)))))
+	    ;; create a prompt from all the interfaces
+	    (dolist (d interfaces) (setq prompt
+					 (format "%s %s" prompt (car d))))
+	    (setq prompt (format "%s%s" prompt " ): "))
+	    ;; get the ip address string from the interface definition
+	    (let* ((chosen (read-from-minibuffer prompt default-device-name))
+		   (ip (cdr (assoc-string chosen interfaces))))
+	      (if (not ip)
+		  (error "could not open interface %s" chosen))
+	      (format "%d.%d.%d.%d"
+		      (aref ip 0) (aref ip 1) (aref ip 2) (aref ip 3))))))
+	  (setq sccollab-server
+		(osc-make-server addr 7777 
+				 (lambda (path &rest args)
+				   (sccollab-receive path args)))))
 
 (defun sccollab-server-stop ()
   "stop the supercollider collaboration"
@@ -40,6 +65,4 @@
   (setq sccollab-clients
 	(mapcar (lambda (client-ip) (osc-make-client client-ip 7777))
 		ip-list)))
-
-
 
